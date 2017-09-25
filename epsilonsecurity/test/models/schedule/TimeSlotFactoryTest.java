c97acbf3f8c4ac026171b44e9d9df5d554e7aa4c
Alexei Popov <apa53@sfu.ca>
2017-09-25 15:22:34 -0700

    changed from Joda to Java.time

--- epsilonsecurity/test/models/schedule/TimeSlotFactoryTest.java
+++ epsilonsecurity/test/models/schedule/TimeSlotFactoryTest.java
@@ -1,28 +1,33 @@
 package models.schedule;
 
-import org.joda.time.DateTime;
 import org.junit.Test;
 
+import java.time.*;
+
+import static java.time.temporal.ChronoUnit.MINUTES;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 
 public class TimeSlotFactoryTest {
 
-    private static DateTime getNoon() {
-        return new DateTime(2017, 1, 1, 12, 0, 0);
+    private static Instant getNoon() {
+        LocalDateTime noon = LocalDateTime.of(LocalDate.of(2017, 1, 1),
+                                              LocalTime.of(12, 0, 0));
+        return noon.toInstant(ZoneOffset.ofHours(0));
     }
 
     @Test
     public void testExactQuantization() {
-        DateTime noon = getNoon();
+
+        Instant noon = getNoon();
         TimeSlot noonTimeSlot = TimeSlotFactory.newTimeSlotFrom(noon);
         assertEquals(noonTimeSlot.getStart(), noon);
     }
 
     @Test
     public void testLowerQuantization() {
-        DateTime noonAndABit = getNoon().plusMinutes(7);
-        DateTime noonAndALot = getNoon().plusMinutes(8);
+        Instant noonAndABit = getNoon().plus(7, MINUTES);
+        Instant noonAndALot = getNoon().plus(8, MINUTES);
         TimeSlot noonTimeSlot = TimeSlotFactory.newTimeSlotFrom(getNoon());
         TimeSlot noonAndABitTimeSlot = TimeSlotFactory.newTimeSlotFrom(noonAndABit);
         TimeSlot noonAndALotTimeSlot = TimeSlotFactory.newTimeSlotFrom(noonAndALot);
@@ -33,8 +38,8 @@ public class TimeSlotFactoryTest {
 
     @Test
     public void testUpperQuantization() {
-        DateTime noon = getNoon();
-        DateTime noonAndALot = getNoon().plusMinutes(8);
+        Instant noon = getNoon();
+        Instant noonAndALot = getNoon().plus(8, MINUTES);
         TimeSlot noonTimeSlot = TimeSlotFactory.newTimeSlotFrom(noon);
         TimeSlot noonAndALotTimeSlot = TimeSlotFactory.newTimeSlotFrom(noonAndALot);
         assertNotEquals(noonTimeSlot.getStart(), noonAndALotTimeSlot.getStart());
