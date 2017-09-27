2eeb709e9c73e9e9210592274e96c2a632d6ad21
Alexei Popov <apa53@sfu.ca>
2017-09-26 22:29:20 -0700

    add Value-semantic equality to TimeBlock

--- epsilonsecurity/app/models/schedule/TimeBlock.java
+++ epsilonsecurity/app/models/schedule/TimeBlock.java
@@ -1,15 +1,18 @@
 package models.schedule;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.sql.Time;
 import java.time.Duration;
 import java.time.Instant;
 
 import java.util.List;
 
-public class TimeBlock {
-    public Instant start;
-    public Instant end;
+import static java.time.temporal.ChronoUnit.MINUTES;
 
-    // TODO: requires initializer
+public class TimeBlock {
+    public final Instant start;
+    public final Instant end;
 
     public Duration getDuration() {
         return Duration.between(start, end);
@@ -19,8 +22,24 @@ public class TimeBlock {
         return TimeSlotFactory.newTimeSlots(start, end);
     }
 
-    public TimeBlock(Instant start, Instant end) {
+    // TODO: quantize? Figure out exactly how this should work and at what access level
+    protected TimeBlock(Instant start, Instant end) {
+        // make sure end is after start?
         this.start = start;
         this.end = end;
+    }
+
+    @NotNull
+    public static TimeBlock single(Instant start) {
+        return new TimeBlock(start, start.plus(TimeSlot.QUANTIZATION_MINUTES, MINUTES));
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if ((obj instanceof TimeBlock) == false) { return false; }
+        TimeBlock other = (TimeBlock) obj;
+        boolean sameStart = other.start.equals(this.start);
+        boolean sameEnd = other.end.equals(this.end);
+        return sameStart && sameEnd;
     }
 }
