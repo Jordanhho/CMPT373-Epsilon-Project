2441e671aad41ff4fa0833ca140869aab2e2c37e
adrielrao <adrielr@sfu.ca>
2017-09-28 00:02:51 -0700

    Implement basic Routes and Functions
    can successfully route some requests from front end to back end now. pair: Nicholas ncfung

--- epsilonsecurity/app/controllers/HomeController.java
+++ epsilonsecurity/app/controllers/HomeController.java
@@ -1,7 +1,9 @@
 package controllers;
 import models.databaseModel.helpers.UserDbHelper;
+import models.databaseModel.scheduling.DbUser;
 import play.mvc.*;
 
+import static models.databaseModel.helpers.UserDbHelper.createDbUser;
 
 
 /**
@@ -18,7 +20,8 @@ public class HomeController extends Controller {
      * this method will be called when the application receives a
      * <code>GET</code> request with a path of <code>/</code>.
      */
-    public Result index() {
+    //public Result index(String path) {
+      public Result index() {
 //        UserDbHelper.createDbUser("three", "four", "five", "six");
 //        UserDbHelper.deleteDbUserById(1);
 //        DbUser dbUser = UserDbHelper.readDbUserById(1);
@@ -26,6 +29,36 @@ public class HomeController extends Controller {
         return ok(views.html.index.render());
     }
 
+    public Result listUsers() {
+        return ok();
+    }
+
+    public Result create(String contactEmail, String sfuEmail, String phoneNumber) {
+        DbUser newUser = new DbUser(contactEmail, sfuEmail, phoneNumber, "url");
+        createDbUser(contactEmail, sfuEmail, phoneNumber, "url");
+        System.out.println(newUser);
+        System.out.println("Successfully created new user");
+        return ok(newUser.toString());
+    }
+
+    public Result test(String contactEmail) {
+        System.out.println(contactEmail);
+        return ok(contactEmail);
+    }
+
+    public Result retrieve(int id) {
+        return ok();
+    }
+
+    public Result update(int id) {
+        return ok();
+    }
+
+    public Result delete(int id) {
+        return ok();
+    }
+
+    // todo: create invalid query request function (all paths in routes file)
 }
 
 
