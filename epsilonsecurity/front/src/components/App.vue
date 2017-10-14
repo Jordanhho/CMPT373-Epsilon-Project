11ce78db1d0d41f8ebfc030cf15734baefd66439
Uzziah Eyee <ueyee@sfu.ca>
2017-10-13 18:59:06 -0700

    WIP: Create basic sections of main frame.
    Created the app header component.
    Pair-programmed with @pkbui (Ken).

--- epsilonsecurity/front/src/components/App.vue
+++ epsilonsecurity/front/src/components/App.vue
@@ -1,36 +1,62 @@
 <template>
-    <div id="hello">
-        <div id="images">
-            <img src="/assets/images/play.png" alt="Play Framework"> +
-            <img src="/assets/images/vue.png" alt="Vue.js">
-        </div>
-				<h1>Congratulations, you now have Vue playing well with Play!</h1>
+    <div id="the-app">
+        <app-header></app-header>
+				<div id="content">
+					<div id="app-sidebar">
+						hello
+					</div>
+					<div id="viewport">
+						world
+					</div>
+				</div>
     </div>
 </template>
 
 <script>
+		import AppHeader from './AppHeader.vue'
     export default {
-
+			components: {
+				AppHeader
+			}
     }
 </script>
 
 <style scoped lang="scss">
-    #hello {
-        width: 600px;
-        margin: 0 auto;
-        color: gray;
-        text-align: center;
-        position: relative;
-        top: 50%;
-        transform: translateY(-50%);
-    }
+	* {
+		padding: 0;
+		margin: 0;
+		box-sizing: border-box;
+	}
+	html, body {
+		width: 100%;
+		height: 100%;
+	}
 
-    #images {
-        font-size: 2.3em;
-        font-weight: bold;
-        margin-bottom: 1em;
-        img {
-            height: 100px;
-        }
-    }
+	#the-app {
+		width: 100%;
+		height: 100%;
+		display: flex;
+		flex-flow: column nowrap;
+	}
+	#app-header {
+		background: red;
+		height: 60px;
+	}
+
+	#content {
+		background: blue;
+		display: flex;
+		flex-flow: row nowrap;
+		flex-grow: 1;
+	}
+
+	#viewport {
+		background: green;
+		flex-grow: 1;
+	}
+
+	#app-sidebar {
+		background: pink;
+		width: 230px;
+	}
 </style>
---
+++ epsilonsecurity/front/src/components/AppHeader.vue
@@ -0,0 +1,64 @@
+<template>
+	<div id="header">
+		<button id="sidebar-btn">
+			<icon name="bars" id="navicon" scale=2.3></icon>
+		</button>
+		<span id="app-name">{{appName}}</span>
+		<span id="spacer"></span>
+		<span id="user-name">{{userName}}</span>
+		<img :src="userPhoto" :alt="userName" id="user-photo">
+	</div>
+</template>
+
+<script>
+
+var Icon = require('vue-awesome')
+
+export default {
+	data () {
+		return {
+			appName: 'Epsilon Security',
+			userName: 'Mark Twain',
+			userPhoto: 'http://lorempixel.com/100/100/people'
+		}
+	},
+	components: {
+    Icon
+  }
+}
+</script>
+
+<style>
+	#header {
+		display: flex;
+		flex-flow: row nowrap;
+		align-items: center;
+		padding: 4px 16px;
+	}
+	#app-name {
+		font-size: 20px;
+	}
+	#spacer {
+		flex-grow: 1;
+	}
+	#user-photo {
+		width: 45px;
+		height: 45px;
+		border-radius: 50%;
+		margin: 0 0 0 8px;
+	}
+	#sidebar-btn {
+		background: none;
+		margin: 0 8px 0 0;
+		outline: none;
+		border: none;
+		display: flex; 
+		flex-flow: row nowrap; 
+		align-items: center;
+	}
+	#navicon {
+		font-size: 20px;
+		padding: 0; margin: 0;
+		height: 100%;
+	}
+</style>
