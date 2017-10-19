5c8588ca090a26c7336b063dab46f528f91837bb
Nicholas Fung <ncfung@sfu.ca>
2017-10-19 11:16:57 -0700

    Add DbRole routes
    Add create, retrieve, delete, and list routes for DbRole.
    Create RoleController to handle route requests.
    Fix retrieve route for DbPermission.

---
+++ epsilonsecurity/app/controllers/RoleController.java
@@ -0,0 +1,25 @@
+package controllers;
+
+import play.mvc.*;
+import models.databaseModel.roles.DbRole;
+import models.databaseModel.helpers.DbRoleHelper;
+
+public class RoleController extends Controller {
+
+//    TODO: Implement functionality of RoleController
+    public Result listRole() {
+        return ok();
+    }
+
+    public Result createRole() {
+        return ok();
+    }
+
+    public Result retrieveRole(Integer RoleId) {
+        return ok();
+    }
+
+    public Result deleteRole(Integer RoleId) {
+        return ok();
+    }
+}
