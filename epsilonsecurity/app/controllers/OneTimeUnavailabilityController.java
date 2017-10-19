9950da15f61222759b82276487ef9528f851297f
Nicholas Fung <ncfung@sfu.ca>
2017-10-19 12:04:49 -0700

    Add DbOneTimeUnavailability routes
    Add create, retrieve, delete, and list routes for DbOneTimeUnavailability.
    Create DbOneTimeUnavailabilityController to handle route requests.

---
+++ epsilonsecurity/app/controllers/OneTimeUnavailabilityController.java
@@ -0,0 +1,29 @@
+package controllers;
+
+import play.mvc.*;
+import models.databaseModel.scheduling.DbOneTimeUnavailability;
+import models.databaseModel.helpers.DbOneTimeUnavailabilityHelper;
+
+public class OneTimeUnavailabilityController extends Controller {
+
+//    TODO: Implement functionality of OneTimeUnavailabilityController
+    public Result listOneTimeUnavailability() {
+        return ok();
+    }
+
+    public Result createOneTimeUnavailability() {
+        return ok();
+    }
+
+    public Result retrieveOneTimeUnavailability(Integer oneTimeUnavailabilityId) {
+        return ok();
+    }
+
+    public Result deleteOneTimeUnavailability(Integer oneTimeUnavailabilityId) {
+        return ok();
+    }
+
+    public Result readOneTimeUnavailabilitiesByTimeRange(Integer timeStart, Integer timeEnd) {
+        return ok();
+    }
+}
