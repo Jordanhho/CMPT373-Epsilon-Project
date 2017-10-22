1399869b23d69376bab3b71295e3b3111dc634bb
adrielrao <adrielr@sfu.ca>
2017-10-22 16:02:31 -0700

    Initial Commit on ManageUsers

---
+++ epsilonsecurity/front/src/components/ManageUsers.vue
@@ -0,0 +1,25 @@
+<template>
+    <div id="manage-users">
+        <h1>TODO: Mervy Schedule Component</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style scoped>
+    #manage-users {
+        background: white;
+        width: 100%;
+        position: relative;
+    }
+    #manage-users h1 {
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%);
+    }
+</style>
--- epsilonsecurity/front/src/router.js
+++ epsilonsecurity/front/src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import MyFeed from './components/MyFeed.vue';
 import MySchedule from './components/MySchedule.vue';
+import ManageUsers from './components/ManageUsers.vue';
 // todo: import other components here
 import NotFound from './components/NotFound.vue';
 
@@ -12,6 +13,8 @@ export default new VueRouter({
   routes: [
 		{ path: '/', component: MyFeed },
 		{ path: '/my-schedule', component: MySchedule },
+		{ path: '/manage-users', component: ManageUsers },
+      	//{ path: '/manage-users', component: ManageUsers },
 		// todo: other routes here
 		{ path: '*', component: NotFound },
   ]
