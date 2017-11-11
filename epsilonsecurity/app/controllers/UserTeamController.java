75c6cb8353dbdf8577433c6f7585be6ca88aced4
Nicholas Fung <ncfung@sfu.ca>
2017-11-11 00:11:22 -0800

    Initialize "assign user to a list of teams" query
    Add a new route to routes file.
    Add ability to use FormFactory in UserTeamController.
    Add a new createUserTeam() polymorph to UserTeamController.

--- epsilonsecurity/app/controllers/UserTeamController.java
+++ epsilonsecurity/app/controllers/UserTeamController.java
@@ -2,20 +2,35 @@ package controllers;
 
 import models.databaseModel.scheduling.DbTeam;
 import models.databaseModel.scheduling.DbUser;
+import play.data.Form;
+import play.data.FormFactory;
 import play.libs.Json;
 import play.mvc.*;
 import models.databaseModel.scheduling.DbUserTeam;
 import models.databaseModel.helpers.DbUserTeamHelper;
 
+import javax.inject.Inject;
 import java.util.List;
 
 public class UserTeamController extends Controller {
+
+    private final FormFactory formFactory;
+
+    @Inject
+    UserTeamController(FormFactory formFactory) {
+        this.formFactory = formFactory;
+    }
+
+    private DbUserTeam getDbUserTeamFromForm() {
+        Form<DbUserTeam> form = formFactory.form(DbUserTeam.class).bindFromRequest();
+        return form.get();
+    }
 
     public Result listUserTeams() {
         return ok();
     }
 
-    public Result createUserTeam() {
+    public Result createUserTeam(Integer userId) {
         return ok();
     }
 
