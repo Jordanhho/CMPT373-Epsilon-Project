5f8c5a17c2f43f5a8dd2eb550b87a7a862a34c98
Jordan Ho <jhho@sfu.ca>
2017-11-16 01:28:04 -0800

    Revert "Merge branch 'revert-656b385b' into 'master'"
    This reverts merge request !42

---
+++ epsilonsecurity/front/config/webpack.dev.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+
+const ENTRY_POINT = '../src/main.js';
+const OUTPUT_DIR = '../../public';
+const DEV_SERVER_PORT = 3000;
+
+module.exports = {
+	entry: path.join(__dirname, ENTRY_POINT),
+	output: {
+		path: path.join(__dirname, OUTPUT_DIR),
+		publicPath: '/',
+		filename: 'bundle.js',
+	},
+	resolve: {
+		extensions: ['.js', '.vue', '.json'],
+		alias: {
+			'vue$': 'vue/dist/vue.esm.js',
+		}
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				loader: 'babel-loader',
+				exclude: /node_modules/
+			},
+			{
+				test: /\.vue$/,
+				loader: 'vue-loader',
+				options: {}
+			},
+			{
+				test: /\.(png|jpg|gif|svg)$/,
+				loader: 'file-loader',
+				options: {
+					name: '[name].[ext]?[hash]'
+				}
+			}
+		]
+	},
+	devServer: {
+		port: DEV_SERVER_PORT,
+	}
+};
