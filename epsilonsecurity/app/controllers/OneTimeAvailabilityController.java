84abe9091378da473e67858d10ae34db5bd1b367
Nicholas Fung <ncfung@sfu.ca>
2017-11-17 19:03:42 -0800

    Implement most of the controller functions
    Implement all functions except createOneTimeAvailability().
    Rename deleteDbOneTimeAvailabilityById() to deleteDbOneTimeAvailability().

--- epsilonsecurity/app/controllers/OneTimeAvailabilityController.java
+++ epsilonsecurity/app/controllers/OneTimeAvailabilityController.java
@@ -1,29 +1,53 @@
 package controllers;
 
-import play.mvc.*;
-import models.databaseModel.scheduling.DbOneTimeAvailability;
 import models.databaseModel.helpers.DbOneTimeAvailabilityHelper;
+import models.databaseModel.scheduling.DbOneTimeAvailability;
+import play.data.FormFactory;
+import play.libs.Json;
+import play.mvc.Controller;
+import play.mvc.Result;
+
+import javax.inject.Inject;
+import java.util.List;
 
 public class OneTimeAvailabilityController extends Controller {
 
-//    TODO: Implement functionality of OneTimeAvailabilityController
+    private static final String TIME_START = "start";
+    private static final String TIME_END = "end";
+    private final FormFactory formFactory;
+
+    @Inject
+    OneTimeAvailabilityController(FormFactory formFactory) {
+        this.formFactory = formFactory;
+    }
+
+
     public Result listOneTimeAvailabilities() {
-        return ok();
+        List<DbOneTimeAvailability> dbOneTimeAvailabilityList = DbOneTimeAvailabilityHelper.readAllDbOneTimeAvailability();
+        return ok(Json.toJson(dbOneTimeAvailabilityList));
     }
 
     public Result createOneTimeAvailability() {
         return ok();
     }
 
-    public Result retrieveOneTimeAvailability(Integer oneTimeAvailabilityId) {
-        return ok();
+    public Result readOneTimeAvailabilityById(Integer id) {
+        DbOneTimeAvailability dbOneTimeAvailability = DbOneTimeAvailabilityHelper.readDbOneTimeAvailabilityById(id);
+        return ok(Json.toJson(dbOneTimeAvailability));
     }
 
-    public Result deleteOneTimeAvailability(Integer oneTimeAvailabilityId) {
+    public Result deleteOneTimeAvailability(Integer id) {
+        DbOneTimeAvailability dbOneTimeAvailability = DbOneTimeAvailabilityHelper.readDbOneTimeAvailabilityById(id);
+        DbOneTimeAvailabilityHelper.deleteDbOneTimeAvailability(dbOneTimeAvailability);
         return ok();
     }
 
     public Result readOneTimeAvailabilitiesByTimeRange() {
-        return ok();
+        Long timeStart = Long.parseLong(request().getQueryString(TIME_START));
+        Long timeEnd = Long.parseLong(request().getQueryString(TIME_END));
+        List<DbOneTimeAvailability> dbOneTimeAvailabilityList =
+                DbOneTimeAvailabilityHelper.readDbOneTimeAvailabilityByTimeRange(timeStart, timeEnd);
+
+        return ok(Json.toJson(dbOneTimeAvailabilityList));
     }
 }
