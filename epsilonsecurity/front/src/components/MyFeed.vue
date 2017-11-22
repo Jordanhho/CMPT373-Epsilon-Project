bd4514548ab5d79ac4060f809908fc1776c33a52
jvocal <jvocal@sfu.ca>
2017-11-22 09:18:25 -0800

    Add multiple schedule reminder components to feed

--- epsilonsecurity/front/src/components/MyFeed.vue
+++ epsilonsecurity/front/src/components/MyFeed.vue
@@ -1,26 +1,37 @@
 <template>
-	<div id="my-feed">
-		<h1>TODO: My Feed Component</h1>
-	</div>
+
+    <div class="my-feed">
+        <ScheduleReminder></ScheduleReminder>
+        <ScheduleReminder></ScheduleReminder>
+        <ScheduleReminder></ScheduleReminder>
+    </div>
+
 </template>
 
 <script>
-export default {
 
-}
+    import ScheduleReminder from './ScheduleReminder.vue'
+
+    export default {
+        name: 'my-feed',
+        data() {
+            return {
+                scheduleReminders: []
+            }
+        },
+        components: {
+            ScheduleReminder
+        }
+    }
+
 </script>
 
 <style scoped>
-#my-feed {
-	background: white;
-	width: 100%;
-	/* height: 100%; */
-	position: relative;
-}
-#my-feed h1 {
-	position: absolute;
-	left: 50%;
-	top: 50%;
-	transform: translate(-50%, -50%);
-}
+
+    .my-feed {
+        height: 100%;
+        margin: auto;
+    }
+
+
 </style>
