00c949735b6e600759afeb339a31867225569ea5
Uzziah Eyee <ueyee@sfu.ca>
2017-11-28 16:27:24 -0800

    Resolved CAS logout issue.
    WIP: fetching the profile of the authenticated user.

--- epsilonsecurity/app/controllers/HomeController.java
+++ epsilonsecurity/app/controllers/HomeController.java
@@ -2,15 +2,30 @@ package controllers;
 
 import models.DummyDatabase.DummyDataBase;
 import models.databaseModel.scheduling.DbUser;
+import org.pac4j.cas.profile.CasProxyProfile;
 import play.Environment;
 import play.mvc.*;
 import views.html.index;
 
+import org.pac4j.core.profile.CommonProfile;
+import org.pac4j.core.profile.ProfileManager;
+import org.pac4j.play.PlayWebContext;
+import org.pac4j.core.config.Config;
+import org.pac4j.play.store.PlaySessionStore;
+
+import play.libs.Json;
+
 import javax.inject.Inject;
+import java.util.List;
 
 public class HomeController extends Controller {
 
     private final Environment env;
+
+    @Inject
+    private Config config;
+    @Inject
+    private PlaySessionStore playSessionStore;
 
     @Inject
     public HomeController(Environment env) {
@@ -23,5 +38,28 @@ public class HomeController extends Controller {
             new DummyDataBase();
         }
         return status(200, index.render(env, 3000, "bundle.js", "bundle.css"));
+    }
+
+    private List<CommonProfile> getProfiles() {
+        final PlayWebContext context = new PlayWebContext(ctx(), playSessionStore);
+        final ProfileManager<CommonProfile> profileManager = new ProfileManager(context);
+        return profileManager.getAll(true);
+    }
+
+//    private Result protectedIndexView() {
+//        return status(200, Json.toJson( getProfiles() ));
+//    }
+
+//    @Secure(clients = "CasClient")
+    public Result casUser() {
+        final CommonProfile profile = getProfiles().get(0);
+//        final String service = "http://localhost:8080/proxiedService";
+//        String proxyTicket = null;
+//        if (profile instanceof CasProxyProfile) {
+//            final CasProxyProfile proxyProfile = (CasProxyProfile) profile;
+//            proxyTicket = proxyProfile.getProxyTicketFor(service);
+//        }
+//        return ok(views.html.casProtectedIndex.render(profile, service, proxyTicket));
+        return status(200, Json.toJson( profile ));
     }
 }
--- epsilonsecurity/front/src/main.js
+++ epsilonsecurity/front/src/main.js
@@ -3,6 +3,7 @@ import router from './router'
 import Vuetify from 'vuetify'
 import { store } from './store'
 import App from './App'
+import axios from 'axios'
 
 Vue.use(Vuetify)
 
@@ -14,6 +15,16 @@ new Vue({
     return h(App);
   },
 	created () {
+		console.log('main> app created!')
+
+		// note: user is presumed authenticated before the SPA loads.
+		// so, fetch user info from backend.
+		// todo: fetch user info from backend
+		console.log('main> making request for auth user...')
+		axios.get('/api/auth-user')
+			.then(response => {
+				console.log(JSON.stringify(response, null, 2))
+			})
 		/*
 		if user is authenticated:
 			- get user info from backend
